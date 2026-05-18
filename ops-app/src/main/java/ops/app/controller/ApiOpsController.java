bc683dd663a47388a5fef6c7f9b68304374ca763
wonhou <wiehwjr3@gmail.com>
2026-05-18 17:31:02 +0900

    update feat

--- ops-app/src/main/java/ops/app/controller/ApiOpsController.java
+++ ops-app/src/main/java/ops/app/controller/ApiOpsController.java
@@ -1,6 +1,5 @@
 package ops.app.controller;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +16,10 @@ public class ApiOpsController {
     @GetMapping("/health")
     public String healthCheck() {
         return "OK\n";
+    }
+
+    @GetMapping("/feat-ops")
+    public String opsCheck() {
+        return "Get - /api/feat-ops";
     }
 }
